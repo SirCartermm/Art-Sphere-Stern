541e1bd17b6c554ff00c970547e64482a00feebb
SirCartermme <samuel.maina1@student.moringaschool.comm>
2024-08-01 13:20:20 +0300

    Modified app.py

--- app/app.py
+++ app/app.py
@@ -4,3 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///art_gallery.db'
+db = SQLAlchemy(app)
