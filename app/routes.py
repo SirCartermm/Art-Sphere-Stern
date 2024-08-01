a4dcef1593be2df3fff6e52be6fa4520dd3077f7
SirCartermme <samuel.maina1@student.moringaschool.comm>
2024-08-01 15:42:54 +0300

    Modified routes.py

--- app/routes.py
+++ app/routes.py
@@ -3,10 +3,21 @@ from app import app, db, ma, jwt
 from models import User, Artwork
 
 @app.route('/auth/signup', methods=['POST'])
-def signup():
+def signup():Let us try and be flexible ,those available at 3 can meet then the major at 8:00
     data = request.get_json()
     user = User(username=data['username'], email=data['email'], role=data['role'])
     user.set_password(data['password'])
     db.session.add(user)
     db.session.commit()
-    return jsonify({'message': 'User created successfully'}, 201)
+    return jsonify({'message': 'User created successfully'}), 201
+
+@app.route('/auth/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    user = User.query.filter_by(username=data['username']).first()
+    if user and user.check_password(data['password']):
+        access_token = jwt.create_access_token(identity=user.id)
+        return jsonify({'access_token': access_token}), 200
+    return jsonify({'message': 'Invalid credentials'}), 401
+
+@app.route('/artwork', methods=['GET'])
