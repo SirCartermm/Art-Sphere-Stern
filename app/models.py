26bd958c972897f4547579b8478254ee863e4f8e
SirCartermme <samuel.maina1@student.moringaschool.comm>
2024-08-01 13:25:32 +0300

    Modified models.py

---
+++ app/models.py
@@ -0,0 +1,7 @@
+# models.py
+from app import db, ma
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, nullable=False)
+    
