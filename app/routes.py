c02f7b7fdf568e8689d78f0981b02b6ed0592b82
SirCartermme <samuel.maina1@student.moringaschool.comm>
2024-08-01 14:37:29 +0300

    Modified models.py

--- app/routes.py
+++ app/routes.py
@@ -6,4 +6,7 @@ from models import User, Artwork
 def signup():
     data = request.get_json()
     user = User(username=data['username'], email=data['email'], role=data['role'])
-    
+    user.set_password(data['password'])
+    db.session.add(user)
+    db.session.commit()
+    return jsonify({'message': 'User created successfully'}, 201)
